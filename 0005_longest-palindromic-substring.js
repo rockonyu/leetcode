340daeb732d75f35258f27e26ce27b600ebe8ad5
austin <rockonyu@gmail.com>
2021-03-01 15:55:39 +0800

    feat: update #5 solution

--- 0005_longest-palindromic-substring.js
+++ 0005_longest-palindromic-substring.js
@@ -5,16 +5,6 @@
 // Output: "bab"
 // Note: "aba" is also a valid answer.
 
-var checkIsPalindrome = (s) => {
-  for (let i = 0; i < s.length / 2; i++) {
-    if (s[i] !== s[s.length - i - 1]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 /**
  * 暴力法: 判斷回文的 checkIsPalindrome 太低效會逾時
  *
@@ -25,6 +15,16 @@ var checkIsPalindrome = (s) => {
  * @return {string}
  */
 var longestPalindrome = function (s) {
+  const checkIsPalindrome = (s) => {
+    for (let i = 0; i < s.length / 2; i++) {
+      if (s[i] !== s[s.length - i - 1]) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
   let result = "";
 
   for (let i = 0; i < s.length; i++) {
@@ -38,4 +38,41 @@ var longestPalindrome = function (s) {
   }
 
   return result;
+};
+
+/**
+ * Expand Around Center
+ *
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ *
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  const getPalindromeAroundLen = (s, left, right) => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+
+    // 計算 left 與 right 之間有幾個元素
+    return right - left - 1;
+  };
+
+  let start = 0;
+  let palindromeLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const oddLength = getPalindromeAroundLen(s, i, i);
+    const eventLength = getPalindromeAroundLen(s, i, i + 1);
+    const largerLength = Math.max(oddLength, eventLength);
+
+    if (largerLength > palindromeLength) {
+      start = i - Math.floor((largerLength - 1) / 2);
+      palindromeLength = largerLength;
+    }
+  }
+
+  return s.substr(start, palindromeLength);
 };
