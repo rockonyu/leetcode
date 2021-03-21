482e0650b1d68c1faf2b24663fb31d254cc03244
austin <rockonyu@gmail.com>
2021-03-21 12:02:59 +0800

    feat: update #22

--- 0022_generate-parentheses.js
+++ 0022_generate-parentheses.js
@@ -1,28 +1,48 @@
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
-const helper = (list, str, open, close, max) => {
-  if (str.length === max * 2) {
-    list.push(str);
-    return;
+const backtrack = (
+  list,
+  currStr,
+  currOpenLength,
+  currCloseLength,
+  halfLength
+) => {
+  if (currStr.length === halfLength * 2) {
+    list.push(currStr);
   }
 
-  if (open < max) {
-    helper(list, `${str}(`, open + 1, close, max);
+  if (currOpenLength < halfLength) {
+    backtrack(
+      list,
+      `${currStr}(`,
+      currOpenLength + 1,
+      currCloseLength,
+      halfLength
+    );
   }
 
-  if (close < open) {
-    helper(list, `${str})`, open, close + 1, max);
+  if (currCloseLength < currOpenLength) {
+    backtrack(
+      list,
+      `${currStr})`,
+      currOpenLength,
+      currCloseLength + 1,
+      halfLength
+    );
   }
 };
 
 /**
+ * Complexity Analysis
+ * https://leetcode.com/problems/generate-parentheses/solution/
+ *
  * @param {number} n
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
   const result = [];
 
-  helper(result, "", 0, 0, n);
+  backtrack(result, "", 0, 0, n);
 
   return result;
 };
