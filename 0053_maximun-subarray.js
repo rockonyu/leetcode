b37d17904f021aa6e40627b3f8e888a999430c01
yuzhang <rockonyu@gmail.com>
2021-04-03 13:42:45 +0800

    feat: update #53

--- 0053_maximun-subarray.js
+++ 0053_maximun-subarray.js
@@ -1,4 +1,7 @@
 /**
+ * Time complexity : O(n^2)
+ * Space complexity : O(1)
+ *
  * @param {number[]} nums
  * @return {number}
  */
@@ -16,4 +19,27 @@ var maxSubArray = function (nums) {
   }
 
   return max;
+};
+
+/**
+ * Dynamic Programming, Kadane's Algorithm
+ * 重點在理解 subSum 內要保存的內容是甚麼，以便後續計算最大值
+ *
+ * Time complexity : O(n)
+ * Space complexity : O(1)
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let maxSum = nums[0];
+  let subSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    // 如果累加的值比目前的值小，就直接拋棄
+    subSum = Math.max(subSum + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, subSum);
+  }
+
+  return maxSum;
 };
